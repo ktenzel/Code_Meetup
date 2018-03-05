66ef5c510744f1062378822a329a5ab307cbeb43
Anduin Mooney <anduinmooney@gmail.com>
2018-03-05 11:48:42 -0800

    Add user server and user-profile component that displays users. Work on displaying individual user.

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,11 +1,16 @@
 import { ModuleWithProviders }  from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
 
 const appRoutes: Routes = [
   {
   path: '',
   component: HomeComponent
+  },
+  {
+  path: 'user-profile',
+  component: UserProfileComponent
   }
 
 ];
