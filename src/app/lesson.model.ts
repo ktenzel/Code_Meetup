57b105693c335b52d6b933f0c3a2538d53397de0
Ross Fletcher <rtbfletch@outlook.com>
2018-03-05 16:15:53 -0800

    add lesson model and mock-lessons.ts

---
+++ src/app/lesson.model.ts
@@ -0,0 +1,5 @@
+ export class Lesson {
+   constructor(public skillLevel: string,
+     public language: string, public reviewMaterial: string, public meetUpDayMaterial: string, public instructions: string
+   ) {}
+}
