d6a14625e3f54491b099f63b0aef910637ce8e23
Anduin Mooney and Kyle Tenzeldam <anduin+kyle@>
2018-03-06 11:51:27 -0800

    Move sign in page to another page, keep navbar working.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,29 +8,5 @@ import { AuthenticationService } from './authentication.service';
   providers: [AuthenticationService]
 })
 export class AppComponent {
-  user;
-  private isLoggedIn: Boolean;
-  private userName: String;
-  private uid: string;
 
-  constructor(public authService: AuthenticationService) {
-    this.authService.user.subscribe(user =>  {
-      if (user == null) {
-        this.isLoggedIn = false;
-      } else {
-        this.isLoggedIn = true;
-        this.userName = user.displayName;
-        this.uid = user.uid;
-        console.log(this.uid);
-      }
-    });
-  }
-
-  login() {
-    this.authService.login();
-  }
-
-  logout() {
-    this.authService.logout();
-  }
 }
---
+++ src/app/sign-in-out/sign-in-out.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from '../authentication.service';
+
+@Component({
+  selector: 'app-sign-in-out',
+  templateUrl: './sign-in-out.component.html',
+  styleUrls: ['./sign-in-out.component.css'],
+  providers: [AuthenticationService]
+})
+export class SignInOutComponent implements OnInit {
+
+  user;
+  private isLoggedIn: Boolean;
+  private userName: String;
+  private uid: string;
+
+  constructor(public authService: AuthenticationService) {
+    this.authService.user.subscribe(user =>  {
+      if (user == null) {
+        this.isLoggedIn = false;
+      } else {
+        this.isLoggedIn = true;
+        this.userName = user.displayName;
+        this.uid = user.uid;
+        console.log(this.uid);
+      }
+    });
+  }
+
+  login() {
+    this.authService.login();
+  }
+
+  logout() {
+    this.authService.logout();
+  }
+  ngOnInit() {
+  }
+
+}
