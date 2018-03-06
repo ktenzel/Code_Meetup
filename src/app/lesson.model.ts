764bd3525e66dc6078407ceab3bcbb590b50665a
Ross Fletcher <rtbfletch@outlook.com>
2018-03-05 16:20:42 -0800

    begin to add getLessons method in lesson.model

--- src/app/lesson.model.ts
+++ src/app/lesson.model.ts
@@ -2,4 +2,10 @@
    constructor(public skillLevel: string,
      public language: string, public reviewMaterial: string, public meetUpDayMaterial: string, public instructions: string
    ) {}
+
+  // getLessonsByLanguage(language) {
+  //   if(language === "HTML/CSS Intro") {
+  //
+  //   }
+  // }
 }
