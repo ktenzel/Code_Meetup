55f86412304afba12e209e0bf0040de3c8200cd4
Abdullah Yahya <33635135+AbdulYahya@users.noreply.github.com>
2018-03-06 23:05:54 -0800

    Cleaned p code a bit

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -1,12 +1,9 @@
 import { Injectable } from '@angular/core';
 import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
-import { User } from './user.model';
-import * as firebase from 'firebase/app';
 
 @Injectable()
 export class UserService {
   users: FirebaseListObservable<any[]>;
-  userExists: Boolean = true;
 
   constructor(private db: AngularFireDatabase) {
     this.users = db.list('users');
