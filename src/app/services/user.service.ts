a116ba71d6449fba2b5217de422706b06325c339
Abdullah Yahya <abdullah@>
2018-03-07 10:43:49 -0800

    Updated Firebase DB function to include user emails. Associated users UID with meetups they create.

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,15 +1,17 @@
 import { Injectable } from '@angular/core';
 import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 
+import { AuthenticationService } from './authentication.service';
+
 @Injectable()
 export class UserService {
   users: FirebaseListObservable<any[]>;
 
-  constructor(private db: AngularFireDatabase) {
+  constructor(private db: AngularFireDatabase, private authService: AuthenticationService) {
     this.users = db.list('users');
   }
 
-  getUsers() {
-    return this.users;
+  getCurrentUserUID() {
+    return this.authService.userDetails.uid;
   }
 }
