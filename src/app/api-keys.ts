72379c0cbe6765d0d00c1fab95ee4b88b2b9912d
Abdullah Yahya <abdullah@>
2018-03-07 11:23:12 -0800

    Updated to new group firebase db

--- src/app/api-keys.ts
+++ src/app/api-keys.ts
@@ -1,7 +1,7 @@
 export const masterFirebaseConfig = {
-  apiKey: "AIzaSyCXc__AAcvlaMiaEkrsjuPJUxaRhRRbgmY",
-  authDomain: "code-day-ay.firebaseapp.com",
-  databaseURL: "https://code-day-ay.firebaseio.com",
-  storageBucket: "code-day-ay.appspot.com",
-  messagingSenderId: "158688350070"
+  apiKey: "AIzaSyChBvYreZ0bZyz14vAApLL7HN8s1nykYb8",
+  authDomain: "codeday-epicodus.firebaseapp.com",
+  databaseURL: "https://codeday-epicodus.firebaseio.com",
+  storageBucket: "codeday-epicodus.appspot.com",
+  messagingSenderId: "614917843578"
 }
