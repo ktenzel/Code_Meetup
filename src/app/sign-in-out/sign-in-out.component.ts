40fb79ec52165184b95d3726cfe056421e0dbedc
Anduin Mooney and Kyle Tenzeldam <anduin+kyle@>
2018-03-06 16:25:20 -0800

    Add required.

--- src/app/sign-in-out/sign-in-out.component.ts
+++ src/app/sign-in-out/sign-in-out.component.ts
@@ -28,7 +28,9 @@ export class SignInOutComponent implements OnInit {
       }
     });
   }
-
+  // createUser(name: string, email: string, password: string, link: string, language: string) {
+  //   var newUser: User = new User(email, password);
+  // } -- Work In Progress 03/06/2018
   // login() {
   //   this.authService.login();
   // }
