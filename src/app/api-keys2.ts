9ad37d0e26a5a114fac6ab60227821df80cd4b16
Guest User <Guest@Epicodus-4D.local>
2018-03-07 09:26:54 -0800

    .

---
+++ src/app/api-keys2.ts
@@ -0,0 +1,8 @@
+export var masterFirebaseConfig = {
+  apiKey: "AIzaSyCs0g6DoYmwJtbRQdD0pa22ptkj2VGwbr0",
+  authDomain: "codemeetup-86187.firebaseapp.com",
+  databaseURL: "https://codemeetup-86187.firebaseio.com",
+  projectId: "codemeetup-86187",
+  storageBucket: "codemeetup-86187.appspot.com",
+  messagingSenderId: "774168781531"
+};
