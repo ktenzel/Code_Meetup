aff0659cbbe62fbbb2c5b9dc7e77f9e8496b91be
Abdullah Yahya <33635135+AbdulYahya@users.noreply.github.com>
2018-03-06 23:00:16 -0800

    Restructured function dir. Removed uneccessary function dirs.

--- functions/index.js
+++ functions/index.js
@@ -5,7 +5,7 @@ admin.initializeApp(functions.config().firebase);
 
 exports.addUserToDB = functions.auth.user().onCreate(event => {
   let timestamp = Date.now();
-  
+
   admin.database().ref('users/' + event.data.uid).set({
     name: event.data.displayName,
     skill: '',
