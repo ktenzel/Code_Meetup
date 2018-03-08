2612efab9e3b12988641810dc8022de0c0eed8c9
Abdullah Yahya <33635135+AbdulYahya@users.noreply.github.com>
2018-03-07 19:47:37 -0800

    Updated custom email/password user creation. Correctly updates user properties after creation

--- src/app/user.model.ts
+++ src/app/user.model.ts
@@ -1,7 +1,7 @@
 export class User {
   constructor (
     public name: string,
-    public skill: string,
-    public language: string
+    public language: string,
+    public url: string
     ) { }
 }
