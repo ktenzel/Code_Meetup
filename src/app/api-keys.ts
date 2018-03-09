f2524a63e8bc639455c6be23294b08a222f2a922
Abdullah Yahya <33635135+AbdulYahya@users.noreply.github.com>
2018-03-09 01:02:39 -0800

    Added api template

---
+++ src/app/api-keys.ts
@@ -0,0 +1,7 @@
+export const masterFirebaseConfig = {
+  apiKey: 'xxx',
+  authDomain: 'xxx.firebaseapp.com',
+  databaseURL: 'https://xxx.firebaseio.com',
+  storageBucket: 'xxx.appspot.com',
+  messagingSenderId: 'xxx'
+};
